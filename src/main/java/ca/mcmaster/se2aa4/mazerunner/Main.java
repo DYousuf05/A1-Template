6bfe66bfcad89547c79ebbea5cf9d8593342cdab
DYousuf05 <danyalyousuf05@gmail.com>
2025-01-23 23:55:36 -0500

    Generate Directions

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -6,61 +6,182 @@ import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.commons.cli.*;
+import java.util.*;
 
 enum Direction {
     NORTH,
     EAST,
     SOUTH,
-    WEST,
+    WEST;
 }
 
 class Maze {
     private boolean[][] maze;
     private int entryPoint;
+    private Player player;
+    public ArrayList<String> path = new ArrayList<String>();
 
     public Maze(int rows, int cols) {
         maze = new boolean[rows][cols];
+        player = new Player();
+        player.setDir(Direction.EAST);
     }
 
     public void locateEntry() {
         for (int row = 0; row < maze.length; row++) {
             if (!maze[row][0]) {
                 entryPoint = row;
+                player.setPos(row, 0);
+                break;
             }
         }
     }
-    
-    public void setMazeUnit(int row, int col, boolean val) {
-        maze[row][col] = val;
+
+    public void traverse() {
+        while (!player.isAtExit(maze)) {
+            if (!player.rightSide(maze)) {
+                player.turnRight();
+                path.add("R");
+                player.move();
+                path.add("F");
+            }
+            else if (!player.forwardSide(maze)) {
+                player.move();
+                path.add("F");
+            }
+            else if (!player.leftSide(maze)) {
+                player.turnLeft();
+                path.add("L");
+                player.move();
+                path.add("F");
+            }
+            else {
+                player.turnRight();
+                path.add("R");
+                player.turnRight();
+                path.add("R");
+                player.move();
+                path.add("F");
+            }
+        }
     }
 
-    public boolean isBlocked(int row, int col) {
-        return maze[row][col] == true;
+    public void setMazeUnit(int row, int col, boolean val) {
+        maze[row][col] = val;
     }
 }
 
 class Player {
     private Direction dir;
-    private Position pos;
+    private int row;
+    private int col;
 
-    public Player() {
+    public boolean isAtExit(boolean[][] maze) {
+        return col == maze[row].length - 1;
+    }
 
+    public void setPos(int newRow, int newCol) {
+        row = newRow;
+        col = newCol;
     }
 
     public void setDir(Direction newDir) {
         dir = newDir;
     }
 
-}
-
-class Position extends Player {
-    int row;
-    int col;
-
-    public void setPos(int newRow, int newCol) {
-        row = newRow;
-        col = newCol;
+    public void move() {
+        if (dir == Direction.NORTH) {
+            row--;
+        }
+        else if (dir == Direction.EAST) {
+            col++;
+        }
+        else if (dir == Direction.SOUTH) {
+            row++;
+        }
+        else if (dir == Direction.WEST) {
+            col--;
+        }
     }
+
+    public boolean rightSide(boolean[][] maze) {
+        if (dir == Direction.NORTH) {
+            return maze[row][col+1];
+        }
+        else if (dir == Direction.EAST) {
+            return maze[row+1][col];
+        }
+        else if (dir == Direction.SOUTH) {
+            return maze[row][col-1];
+        }
+        else if (dir == Direction.WEST) {
+            return maze[row-1][col];
+        }
+        return true;
+    }
+
+    public boolean leftSide(boolean[][] maze) {
+        if (dir == Direction.NORTH) {
+            return maze[row][col-1];
+        }
+        else if (dir == Direction.EAST) {
+            return maze[row-1][col];
+        }
+        else if (dir == Direction.SOUTH) {
+            return maze[row][col+1];
+        }
+        else if (dir == Direction.WEST) {
+            return maze[row+1][col];
+        }
+        return true;
+    }
+
+    public boolean forwardSide(boolean[][] maze) {
+        if (dir == Direction.NORTH) {
+            return maze[row-1][col];
+        }
+        else if (dir == Direction.EAST) {
+            return maze[row][col+1];
+        }
+        else if (dir == Direction.SOUTH) {
+            return maze[row+1][col];
+        }
+        else if (dir == Direction.WEST) {
+            return maze[row][col-1];
+        }
+        return true;
+    }
+
+    public void turnRight() {
+        if (dir == Direction.NORTH) {
+            dir = Direction.EAST;
+        }
+        else if (dir == Direction.EAST) {
+            dir = Direction.SOUTH;
+        }
+        else if (dir == Direction.SOUTH) {
+            dir = Direction.WEST;
+        }
+        else if (dir == Direction.WEST) {
+            dir = Direction.NORTH;
+        }
+    }
+
+    public void turnLeft() {
+        if (dir == Direction.NORTH) {
+            dir = Direction.WEST;
+        }
+        else if (dir == Direction.WEST) {
+            dir = Direction.SOUTH;
+        }
+        else if (dir == Direction.SOUTH) {
+            dir = Direction.EAST;
+        }
+        else if (dir == Direction.EAST) {
+            dir = Direction.NORTH;
+        }
+    }
+
 }
 
 public class Main {
@@ -68,7 +189,7 @@ public class Main {
     private static final Logger logger = LogManager.getLogger();
     private static final Option mazeArg = new Option("i", "input", true, "Enter a maze");
        public static void main(String[] args) {
-        Player player = new Player();
+        
         
     
         logger.info("** Starting Maze Runner");
@@ -108,7 +229,8 @@ public class Main {
                 }
                 
                 maze.locateEntry();
-
+                maze.traverse();
+                System.out.println(maze.path);
             } 
         } catch(Exception e) {
             logger.error("/!\\ An error has occured /!\\");
